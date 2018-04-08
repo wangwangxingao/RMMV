3e0dc565e97fce1f8079bc1a3ccc90f6f1adfd0d
汪汪 <2335937510@qq.com>
2018-04-08 15:20:08 +0800

    注释添加

--- 注释/rpg_sprites/17 Spriteset_Map.js
+++ 注释/rpg_sprites/17 Spriteset_Map.js
@@ -57,9 +57,13 @@ Spriteset_Map.prototype.update = function() {
 /**隐藏人物 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.hideCharacters = function() {
+    //循环 (开始时 i = 0 ; 当 i <  人物精灵组 长度 ;每一次 i++)
     for (var i = 0; i < this._characterSprites.length; i++) {
+        //精灵 = 人物精灵组[i]
         var sprite = this._characterSprites[i];
+        //如果(不是 精灵 是图块())
         if (!sprite.isTile()) {
+            //精灵 隐藏()
             sprite.hide();
         }
     }
@@ -68,45 +72,66 @@ Spriteset_Map.prototype.hideCharacters = function() {
 /**创建远景图 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.createParallax = function() {
+    //远景图 = 新 平铺精灵()
     this._parallax = new TilingSprite();
+    //远景图 移动(0,0,图像 宽,图像高)
     this._parallax.move(0, 0, Graphics.width, Graphics.height);
+    //基础精灵 添加子项(远景图)
     this._baseSprite.addChild(this._parallax);
 };
 /**----------------------------------------------------------------------------- */
 /**创建图块地图 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.createTilemap = function() {
+    //如果(图像 是webgl())
     if (Graphics.isWebGL()) {
+        //图块地图 = 新 着色器图块地图()
         this._tilemap = new ShaderTilemap();
+    //否则
     } else {
+        //图块地图 = 新 图块地图()
         this._tilemap = new Tilemap();
     }
+    //图块地图 图块宽 = 游戏地图 图块宽()
     this._tilemap.tileWidth = $gameMap.tileWidth();
+    //图块地图 图块高 = 游戏地图 图块高()
     this._tilemap.tileHeight = $gameMap.tileHeight();
+    //图块地图 设置地图(游戏地图 宽() ,游戏地图 高(),游戏地图 数据())
     this._tilemap.setData($gameMap.width(), $gameMap.height(), $gameMap.data());
+    //图块地图 横循环 = 游戏地图 是横循环()
     this._tilemap.horizontalWrap = $gameMap.isLoopHorizontal();
+    //图块地图 纵循环 = 游戏地图 是纵循环()
     this._tilemap.verticalWrap = $gameMap.isLoopVertical();
+    //读取图块设置()
     this.loadTileset();
+    //基础精灵 添加子项(图块精灵)
     this._baseSprite.addChild(this._tilemap);
 };
 /**----------------------------------------------------------------------------- */
 /**读取图块设置 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.loadTileset = function() {
+    //图块设置 = 游戏地图 图块设置()
     this._tileset = $gameMap.tileset();
+    //如果(摊图块设置)
     if (this._tileset) {
         //读取图块设置名字组 = 图块设置 图块设置名称组
         var tilesetNames = this._tileset.tilesetNames;
+        //循环 (开始时 i = 0 ; 当 i <  图块设置名称组 长度 ;每一次 i++)
         for (var i = 0; i < tilesetNames.length; i++) {
-            //图块地图 位图[i] = 
+            //图块地图 位图[i] = 图像管理器 读取图块设置(图块设置名称组[i])
             this._tilemap.bitmaps[i] = ImageManager.loadTileset(tilesetNames[i]);
         }
-        //新图块设置标志组 = 
+        //新图块设置标志组 = 游戏地图 图块设置标志组()
         var newTilesetFlags = $gameMap.tilesetFlags();
+        //图块地图 刷新图块设置()
         this._tilemap.refreshTileset();
+        //如果(不是 图块地图 标志组 等于 (新图块地图标志组 ) )
         if (!this._tilemap.flags.equals(newTilesetFlags)) {
+            //图块地图 刷新()
             this._tilemap.refresh();
         }
+        //图块地图 标志组 = 新图块地图标志组
         this._tilemap.flags = newTilesetFlags;
     }
 };
@@ -114,26 +139,28 @@ Spriteset_Map.prototype.loadTileset = function() {
 /**创建人物 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.createCharacters = function() {
-    //人物精灵 = []
+    //人物精灵组 = []
     this._characterSprites = [];
-
-    //添加事件 到 人物精灵
+    //游戏地图 事件组 对每一个(事件)  
     $gameMap.events().forEach(function(event) {
+        //人物精灵组 添加(新 精灵人物(事件))
         this._characterSprites.push(new Sprite_Character(event));
     }, this);
-    //添加交通工具 到 人物精灵
+    //游戏地图 交通工具组 对每一个(交通工具)  
     $gameMap.vehicles().forEach(function(vehicle) {
+        //人物精灵组 添加(新 精灵人物(交通工具))
         this._characterSprites.push(new Sprite_Character(vehicle));
     }, this);
-    //添加从者 到 人物精灵
+    //游戏游戏者 随从组() 倒序每一个 (随从)
     $gamePlayer.followers().reverseEach(function(follower) {
+        //人物精灵组 添加(新 精灵人物(随从))
         this._characterSprites.push(new Sprite_Character(follower));
     }, this);
-    //添加游戏者 到 人物精灵
+    //人物精灵组 添加(新 精灵人物(游戏游戏者))
     this._characterSprites.push(new Sprite_Character($gamePlayer));
-
-    //循环人物精灵
+    //循环 (开始时 i = 0 ; 当 i <  人物精灵组 长度 ;每一次 i++)
     for (var i = 0; i < this._characterSprites.length; i++) {
+        //图块地图 添加子项(人物精灵组[i])
         this._tilemap.addChild(this._characterSprites[i]);
     }
 };
@@ -141,33 +168,46 @@ Spriteset_Map.prototype.createCharacters = function() {
 /**创建阴影 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.createShadow = function() {
+    //阴影精灵 = 新 精灵()
     this._shadowSprite = new Sprite();
+    //阴影精灵 位图 = 图像管理器 读取系统("Shadow1")
     this._shadowSprite.bitmap = ImageManager.loadSystem('Shadow1');
+    //阴影精灵 锚点 x = 0.5
     this._shadowSprite.anchor.x = 0.5;
+    //阴影精灵 锚点 y = 1
     this._shadowSprite.anchor.y = 1;
+    //阴影精灵 z = 6  
     this._shadowSprite.z = 6;
+    //图块地图 添加子项(阴影精灵)
     this._tilemap.addChild(this._shadowSprite);
 };
 /**----------------------------------------------------------------------------- */
 /**创建目的地 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.createDestination = function() {
+    //目的地精灵 = 新 精灵目的地()
     this._destinationSprite = new Sprite_Destination();
+    //目的地精灵 z = 9
     this._destinationSprite.z = 9;
+    //图块地图 添加子项(目的地精灵)
     this._tilemap.addChild(this._destinationSprite);
 };
 /**----------------------------------------------------------------------------- */
 /**创建天气 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.createWeather = function() {
+    //天气 = 新 天气()
     this._weather = new Weather();
+    //添加子项(天气)
     this.addChild(this._weather);
 };
 /**----------------------------------------------------------------------------- */
 /**更新图块设置 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.updateTileset = function() {
+    //如果(图块设置 !== 游戏地图 图块设置() )
     if (this._tileset !== $gameMap.tileset()) {
+        //读取图块设置()
         this.loadTileset();
     }
 };
@@ -179,27 +219,41 @@ Spriteset_Map.prototype.updateTileset = function() {
  * Simple fix for canvas parallax issue, destroy old parallax and readd to the tree.
  */
 Spriteset_Map.prototype._canvasReAddParallax = function() {
+    //索引 = 基础精灵 子组 索引在(远景图)
     var index = this._baseSprite.children.indexOf(this._parallax);
+    //基础精灵 移除子项(远景图)
     this._baseSprite.removeChild(this._parallax);
+    //远景图 = 新 平铺精灵()
     this._parallax = new TilingSprite();
+    //远景图 移动(0,0,图像 宽,图像高)
     this._parallax.move(0, 0, Graphics.width, Graphics.height);
+    //远景图 位图 =  图像管理器 读取远景图(远景图名称)
     this._parallax.bitmap = ImageManager.loadParallax(this._parallaxName);
+    //基础精灵 添加子项在(远景图 , 索引)
     this._baseSprite.addChildAt(this._parallax, index);
 };
 /**----------------------------------------------------------------------------- */
 /**更新远景图 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.updateParallax = function() {
+    //如果(远景图名称 !== 游戏地图 远景图名称())
     if (this._parallaxName !== $gameMap.parallaxName()) {
+        //远景图名称 = 游戏地图 远景图名称()
         this._parallaxName = $gameMap.parallaxName();
+        //如果(远景图 位图 并且 图像 是webgl()!= true  )
         if (this._parallax.bitmap && Graphics.isWebGL() != true) {
+            //画布重新添加远景图()
             this._canvasReAddParallax();
         } else {
+            //远景图 位图  =  图像管理器 读取远景图(远景图名称)
             this._parallax.bitmap = ImageManager.loadParallax(this._parallaxName);
         }
     }
+    //如果(远景图 位图)
     if (this._parallax.bitmap) {
+        //远景图 原点 x = 游戏地图 远景图ox()
         this._parallax.origin.x = $gameMap.parallaxOx();
+        //远景图 原点 y = 游戏地图 远景图oy()
         this._parallax.origin.y = $gameMap.parallaxOy();
     }
 };
@@ -207,16 +261,22 @@ Spriteset_Map.prototype.updateParallax = function() {
 /**更新图块地图 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.updateTilemap = function() {
+    //图块地图 原点 x = 游戏地图 显示x() * 游戏地图 图块宽()
     this._tilemap.origin.x = $gameMap.displayX() * $gameMap.tileWidth();
+    //图块地图 原点 y = 游戏地图 显示y() * 游戏地图 图块高()
     this._tilemap.origin.y = $gameMap.displayY() * $gameMap.tileHeight();
 };
 /**----------------------------------------------------------------------------- */
 /**更新阴影 */
 /**----------------------------------------------------------------------------- */
 Spriteset_Map.prototype.updateShadow = function() {
+    //天空船 = 游戏地图 天空船()
     var airship = $gameMap.airship();
+    //阴影精灵 x = 天空船 阴影x()
     this._shadowSprite.x = airship.shadowX();
+    //阴影精灵 y = 天空船 阴影y()
     this._shadowSprite.y = airship.shadowY();
+    //阴影精灵 透明度 = 天空船 阴影透明度)
     this._shadowSprite.opacity = airship.shadowOpacity();
 };
 /**----------------------------------------------------------------------------- */
--- 注释/rpg_sprites/18 Spriteset_Battle.js
+++ 注释/rpg_sprites/18 Spriteset_Battle.js
@@ -16,6 +16,7 @@ Spriteset_Battle.prototype.constructor = Spriteset_Battle;
 Spriteset_Battle.prototype.initialize = function() {
     //精灵组基础 初始化 呼叫(this)
     Spriteset_Base.prototype.initialize.call(this);
+    //
     this._battlebackLocated = false;
 };
 /**创建较下的层 */
@@ -81,77 +82,120 @@ Spriteset_Battle.prototype.updateBattleback = function() {
 };
 /**设于战斗背景 */
 Spriteset_Battle.prototype.locateBattleback = function() {
+    //宽 = 战斗区域 宽
     var width = this._battleField.width;
+    //高 = 战斗区域 高
     var height = this._battleField.height;
+    //精灵1 = 背景1精灵
     var sprite1 = this._back1Sprite;
+    //精灵2 = 背景2精灵
     var sprite2 = this._back2Sprite;
+    //精灵1 原点 x = 精灵1 x + (精灵1 位图 宽 - 宽 ) / 2
     sprite1.origin.x = sprite1.x + (sprite1.bitmap.width - width) / 2;
+    //精灵2 原点 x = 精灵1 y + (精灵2 位图 宽 - 宽 ) / 2
     sprite2.origin.x = sprite1.y + (sprite2.bitmap.width - width) / 2;
+    //如果(游戏系统 是侧视())
     if ($gameSystem.isSideView()) {
+        //精灵1 原点 y = 精灵1 y + 精灵1 位图 高 - 高
         sprite1.origin.y = sprite1.x + sprite1.bitmap.height - height;
+        //精灵2 原点 y = 精灵1 y + 精灵2 位图 高 - 高
         sprite2.origin.y = sprite1.y + sprite2.bitmap.height - height;
     }
 };
 /**战斗背景1位图 */
 Spriteset_Battle.prototype.battleback1Bitmap = function() {
+    //返回 图像管理器 读取战斗背景1(战斗背景1名称())
     return ImageManager.loadBattleback1(this.battleback1Name());
 };
 /**战斗背景2位图 */
 Spriteset_Battle.prototype.battleback2Bitmap = function() {
+    //返回 图像管理器 读取战斗背景2(战斗背景2名称())
     return ImageManager.loadBattleback2(this.battleback2Name());
 };
 /**战斗背景1名称 */
 Spriteset_Battle.prototype.battleback1Name = function() {
+    //如果(战斗管理器 是战斗测试())
     if (BattleManager.isBattleTest()) {
+        //返回 数据系统 战斗背景1名称
         return $dataSystem.battleback1Name;
+    //否则 如果(游戏地图 战斗背景1名称())
     } else if ($gameMap.battleback1Name()) {
+        //返回 游戏地图 战斗背景1名称()
         return $gameMap.battleback1Name();
+    //否则 如果(游戏地图 是大地图())
     } else if ($gameMap.isOverworld()) {
+        //返回 大地图战斗背景1名称()
         return this.overworldBattleback1Name();
+    //否则
     } else {
+        //返回 ""
         return '';
     }
 };
 /**战斗背景2名称 */
 Spriteset_Battle.prototype.battleback2Name = function() {
+    //如果(战斗管理器 是战斗测试())
     if (BattleManager.isBattleTest()) {
+        //返回 数据系统 战斗背景2名称
         return $dataSystem.battleback2Name;
+    //否则 如果(游戏地图 战斗背景2名称())
     } else if ($gameMap.battleback2Name()) {
+        //返回 游戏地图 战斗背景2名称()
         return $gameMap.battleback2Name();
+    //否则 如果(游戏地图 是大地图())
     } else if ($gameMap.isOverworld()) {
+        //返回 大地图战斗背景2名称()
         return this.overworldBattleback2Name();
+    //否则
     } else {
+        //返回 ""
         return '';
     }
 };
-/**上层世界战斗背景1名称 */
+/**大地图战斗背景1名称 */
 Spriteset_Battle.prototype.overworldBattleback1Name = function() {
+    //如果(游戏地图 战斗背景1名称() === "" ) 返回 ""
     if ($gameMap.battleback1Name() === '') return '';
+    //如果(游戏游戏者 是在交通工具())
     if ($gamePlayer.isInVehicle()) {
+        //返回  船战斗背景1名称()
         return this.shipBattleback1Name();
+    //否则
     } else {
+        //返回 正常战斗背景1名称()
         return this.normalBattleback1Name();
     }
 };
-/**上层世界战斗背景2名称 */
+/**大地图战斗背景2名称 */
 Spriteset_Battle.prototype.overworldBattleback2Name = function() {
+    //如果(游戏地图 战斗背景2名称() === "" ) 返回 ""
     if ($gameMap.battleback2Name() === '') return '';
+    //如果(游戏游戏者 是在交通工具())
     if ($gamePlayer.isInVehicle()) {
+        //返回  船战斗背景2名称()
         return this.shipBattleback2Name();
+    //否则
     } else {
+        //返回 正常战斗背景2名称()
         return this.normalBattleback2Name();
     }
 };
 /**正常战斗背景1名称 */
 Spriteset_Battle.prototype.normalBattleback1Name = function() {
+    //返回 (范围战斗背景1名称(自动图块种类(1)) 或者
     return (this.terrainBattleback1Name(this.autotileType(1)) ||
+        //范围战斗背景1名称(自动图块种类(0)) 或者 
         this.terrainBattleback1Name(this.autotileType(0)) ||
+        // 默认战斗背景1名称() )
         this.defaultBattleback1Name());
 };
 /**正常战斗背景2名称 */
 Spriteset_Battle.prototype.normalBattleback2Name = function() {
+    //返回 (范围战斗背景2名称(自动图块种类(1)) 或者
     return (this.terrainBattleback2Name(this.autotileType(1)) ||
+        //范围战斗背景2名称(自动图块种类(0)) 或者 
         this.terrainBattleback2Name(this.autotileType(0)) ||
+        // 默认战斗背景2名称() )
         this.defaultBattleback2Name());
 };
 /**范围战斗背景1名称  */
@@ -231,67 +275,95 @@ Spriteset_Battle.prototype.shipBattleback2Name = function() {
 };
 /**自动图块种类 */
 Spriteset_Battle.prototype.autotileType = function(z) {
+    //返回 游戏地图 自动图块种类(游戏游戏者 x , 游戏游戏者 y, z )
     return $gameMap.autotileType($gamePlayer.x, $gamePlayer.y, z);
 };
 /**创建敌人 */
 Spriteset_Battle.prototype.createEnemies = function() {
+    //敌人组 = 游戏敌群 成员组()
     var enemies = $gameTroop.members();
+    //精灵组 = []
     var sprites = [];
+    //循环 (开始时 i = 0 ; 当 i <  敌人组 长度 ;每一次 i++)
     for (var i = 0; i < enemies.length; i++) {
+        //精灵组[i] = 新 精灵敌人(敌人组[i])
         sprites[i] = new Sprite_Enemy(enemies[i]);
     }
+    //精灵组排序 (比较敌人精灵 绑定(this) )
     sprites.sort(this.compareEnemySprite.bind(this));
+    //循环 (开始时 j = 0 ; 当 j <  精灵组 长度 ;每一次 j++)
     for (var j = 0; j < sprites.length; j++) {
+        //战斗区域 添加子项(精灵组[j])
         this._battleField.addChild(sprites[j]);
     }
+    //敌人精灵组 = 精灵组
     this._enemySprites = sprites;
 };
 /**比较敌人精灵 */
 Spriteset_Battle.prototype.compareEnemySprite = function(a, b) {
+    //如果(a.y !== b.y)
     if (a.y !== b.y) {
+        //返回 a.y !== b.y
         return a.y - b.y;
+    //否则
     } else {
+        //返回 b 精灵id  - a 精灵id
         return b.spriteId - a.spriteId;
     }
 };
 /**创建角色 */
 Spriteset_Battle.prototype.createActors = function() {
+    //角色精灵组 = []
     this._actorSprites = [];
+    //循环 (开始时 i = 0 ; 当 i <  游戏队伍 最大战斗成员数() ;每一次 i++)
     for (var i = 0; i < $gameParty.maxBattleMembers(); i++) {
+        //角色精灵组[i] = 新 精灵角色()
         this._actorSprites[i] = new Sprite_Actor();
+        //战斗区域 添加子项(角色精灵组[i])
         this._battleField.addChild(this._actorSprites[i]);
     }
 };
 /**更新角色 */
 Spriteset_Battle.prototype.updateActors = function() {
+    //成员组 = 游戏队伍 战斗成员组()
     var members = $gameParty.battleMembers();
+    //循环 (开始时 i = 0 ; 当 i <  角色精灵组 长度 ;每一次 i++)
     for (var i = 0; i < this._actorSprites.length; i++) {
+        //角色精灵组[i] 设置战斗者(角色[i])       
         this._actorSprites[i].setBattler(members[i]);
     }
 };
 /**战斗者精灵组 */
 Spriteset_Battle.prototype.battlerSprites = function() {
+    //返回 敌人精灵组 连接(角色精灵组)
     return this._enemySprites.concat(this._actorSprites);
 };
 /**是动画播放中 */
 Spriteset_Battle.prototype.isAnimationPlaying = function() {
+    //返回 战斗精灵组() 一些 (精灵)
     return this.battlerSprites().some(function(sprite) {
+        //返回 精灵 是动画播放中()
         return sprite.isAnimationPlaying();
     });
 };
 /**是效果中 */
 Spriteset_Battle.prototype.isEffecting = function() {
+    //返回 战斗精灵组() 一些 (精灵)
     return this.battlerSprites().some(function(sprite) {
+        //返回 精灵 是效果中()    
         return sprite.isEffecting();
     });
 };
 /**是任何一个移动中 */
 Spriteset_Battle.prototype.isAnyoneMoving = function() {
+    //返回 战斗精灵组() 一些 (精灵)
     return this.battlerSprites().some(function(sprite) {
+        //返回 精灵 是移动中()
         return sprite.isMoving();
     });
 };
 /**是忙碌 */
 Spriteset_Battle.prototype.isBusy = function() {
+    //返回 是动画播放中() 或者 是任何一个移动中()
     return this.isAnimationPlaying() || this.isAnyoneMoving();
 };
